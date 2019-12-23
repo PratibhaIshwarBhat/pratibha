9b8ed38a1e72c3b0163e11dafff0bb88130d08e5
slk <slk@WSLKCMP4FT2-01.SLKSOFT.COM>
2019-12-23 09:53:24 +0530

    first change

---
+++ first/src/first1/fi.java
@@ -0,0 +1,11 @@
+package first1;
+
+public class fi {
+
+	public static void main(String[] args) {
+    System.out.println("hello pratibha");
+    
+    System.out.println("hello pratibha");
+	}
+
+}
